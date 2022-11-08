f4f9427ded0f46d19a3c0454f7966af83673105e
Azizbek <azizbekphd@gmail.com>
2022-11-09 01:05:26 +0500

    config/index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://amir-group.vercel.app';
+// export const server = dev ? 'http://localhost:3000' : 'https://amir-group.vercel.app';
+export const server = 'http://localhost:3000';
